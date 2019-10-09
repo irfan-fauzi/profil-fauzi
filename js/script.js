0a3912007fef8a567a1e23f6cc10f0957a4112c0
irfan24 <irfanfauzi24@yahoo.com>
2019-10-09 10:15:24 +0700

    portfolio versi 1

---
+++ js/script.js
@@ -0,0 +1,48 @@
+// event pada saat di klik
+$('.page-scroll').on('click', function(e){
+
+	//ambil isi href
+	var tujuan = $(this).attr('href');
+
+	var elemenTujuan = $(tujuan);
+	
+	$('html , body').animate({
+		scrollTop: elemenTujuan.offset().top - 50
+	}, 1250, 'easeInOutExpo');
+
+	e.preventDefault();
+
+
+});
+
+// menyimpan paralax
+$(window).scroll(function(){
+
+	// membuat efek text h1 dengan kecepatan berbeda dengan jumbotron ketika di scroll
+	// untuk membuat itu, kita harus tau posisi scroll berapa, jarak elemen dengan posisi paling atas browser 
+	
+	// paralax gambar profile
+	const wScroll = $(this).scrollTop();
+	$('.jumbotron img').css({
+
+		'transform' : 'translate(0px, '+ wScroll/4 + '%)'
+
+	});
+
+
+	// paralax h1
+	$('.jumbotron h1').css({
+
+		'transform' : 'translate(0px, '+ wScroll/2 + '%)'
+
+	});
+
+	// portfolio
+	if ( wScroll > $('.portfolio').offset().top - 250 ) {
+
+		console.log('ya');
+	}
+
+
+
+});
